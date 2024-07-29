b24e119ef0b5c469c89d0595073e026dd484f00a
ihsanabuhanifah <ihsanabuhanifah@gmail.com>
2024-07-30 05:35:50 +0700

    halaman pg

--- src/pages/siswa/ujian/Pg.jsx
+++ src/pages/siswa/ujian/Pg.jsx
@@ -1,54 +1,113 @@
 import htmr from "htmr";
 
-export default function Pg({ soals,item, setPayload , payload }) {
+export default function Pg({ soals, item, setPayload, payload }) {
   return (
     <div className="space-y-5">
       <div className="border-b-2 pb-2">{htmr(`<div>${soals.soal}</div>`)}</div>
 
       <div className="relative pl-12">
-        <button onClick={()=> {
+        <button
+          onClick={() => {
+            const soal = payload.data.filter((x) => x.id === item.id);
+            const index = payload.data.findIndex((x) => x.id === item.id);
 
-            const soal = payload.data.filter((x)=>  x.id === item.id)
-            const index = payload.data.findIndex((x)=>  x.id === item.id)
-        
-            console.log('sssss', soal)
-            console.log('xx', index)
-            console.log('dddas', payload)
-            setPayload((s)=> {
-                return {
-                    ...s,
-                    data : s.data
-                }
-            })
-        }} className="absolute  left-0  px-1 py-0 border-2 rounded-full text-gray-400">
+            setPayload((s) => {
+              s.data[index] = {
+                ...soal,
+                jawaban: "A",
+              };
+              return {
+                ...s,
+                data: s.data,
+              };
+            });
+          }}
+          className="absolute  left-0  px-1 py-0 border-2 rounded-full text-gray-400"
+        >
           A
         </button>
         <div>{htmr(`<div>${soals.a}</div>`)}</div>
       </div>
 
       <div className="relative pl-12">
-        <button className="absolute  left-0  px-1 py-0 border-2 rounded-full text-gray-400">
+        <button  onClick={() => {
+            const soal = payload.data.filter((x) => x.id === item.id);
+            const index = payload.data.findIndex((x) => x.id === item.id);
+
+            setPayload((s) => {
+              s.data[index] = {
+                ...soal,
+                jawaban: "B",
+              };
+              return {
+                ...s,
+                data: s.data,
+              };
+            });
+          }} className="absolute  left-0  px-1 py-0 border-2 rounded-full text-gray-400">
           B
         </button>
         <div>{htmr(`<div>${soals.b}</div>`)}</div>
       </div>
 
       <div className="relative pl-12">
-        <button className="absolute  left-0  px-1 py-0 border-2 rounded-full text-gray-400">
+        <button  onClick={() => {
+            const soal = payload.data.filter((x) => x.id === item.id);
+            const index = payload.data.findIndex((x) => x.id === item.id);
+
+            setPayload((s) => {
+              s.data[index] = {
+                ...soal,
+                jawaban: "C",
+              };
+              return {
+                ...s,
+                data: s.data,
+              };
+            });
+          }} className="absolute  left-0  px-1 py-0 border-2 rounded-full text-gray-400">
           C
         </button>
         <div>{htmr(`<div>${soals.c}</div>`)}</div>
       </div>
 
       <div className="relative pl-12">
-        <button className="absolute  left-0  px-1 py-0 border-2 rounded-full text-gray-400">
+        <button  onClick={() => {
+            const soal = payload.data.filter((x) => x.id === item.id);
+            const index = payload.data.findIndex((x) => x.id === item.id);
+
+            setPayload((s) => {
+              s.data[index] = {
+                ...soal,
+                jawaban: "D",
+              };
+              return {
+                ...s,
+                data: s.data,
+              };
+            });
+          }} className="absolute  left-0  px-1 py-0 border-2 rounded-full text-gray-400">
           D
         </button>
         <div>{htmr(`<div>${soals.d}</div>`)}</div>
       </div>
 
       <div className="relative pl-12">
-        <button className="absolute  left-0  px-1 py-0 border-2 rounded-full text-gray-400">
+        <button  onClick={() => {
+            const soal = payload.data.filter((x) => x.id === item.id);
+            const index = payload.data.findIndex((x) => x.id === item.id);
+
+            setPayload((s) => {
+              s.data[index] = {
+                ...soal,
+                jawaban: "E",
+              };
+              return {
+                ...s,
+                data: s.data,
+              };
+            });
+          }} className="absolute  left-0  px-1 py-0 border-2 rounded-full text-gray-400">
           E
         </button>
         <div>{htmr(`<div>${soals.e}</div>`)}</div>
