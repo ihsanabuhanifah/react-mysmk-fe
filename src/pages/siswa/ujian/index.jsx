3a5028c6a38022806d2cd24d05f91d2a070ae813
IkkYa <akunikky11@gmail.com>
2024-08-05 14:21:07 +0700

    update

--- src/pages/siswa/ujian/index.jsx
+++ src/pages/siswa/ujian/index.jsx
@@ -11,8 +11,7 @@ export default function UjianSiswa() {
 
   return (
     <LayoutSiswa title="Exam">
-      {JSON.stringify(examActive)}
-      <section className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-5">
+      <section className="flex flex-col gap-y-4 overflow-y-auto pb-10 w-full h-full pl-2 pr-5">
         {examActive ? (
           <ExamPage examActive={examActive} setExamActive={setExamActive} />
         ) : (
@@ -21,7 +20,6 @@ export default function UjianSiswa() {
               data.data.rows.map((item, index) => (
                <React.Fragment key={index}>
                  <Card
-                 
                  item={item}
                  handleExam={() => {
                    console.log("jalan");
