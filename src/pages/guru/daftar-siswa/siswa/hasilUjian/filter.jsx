77a1c8f02effff377f8f696f9ace4cf88d54f145
faiq28 <faiqafifalthf@gmail.com>
2024-10-28 11:16:05 +0700

    finishing Hasil Ujian di Guru

--- src/pages/guru/daftar-siswa/siswa/hasilUjian/filter.jsx
+++ src/pages/guru/daftar-siswa/siswa/hasilUjian/filter.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import { Select, Button, Icon, Form } from "semantic-ui-react";
 import { Formik } from "formik";
