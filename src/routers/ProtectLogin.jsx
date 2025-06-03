4892be8f322a8d151fcd97cc6e25088068b85ce1
ihsanabuhanifah <ihsanabuhanifah@gmail.com>
2025-06-03 21:50:16 +0700

    update

--- src/routers/ProtectLogin.jsx
+++ src/routers/ProtectLogin.jsx
@@ -5,7 +5,10 @@ import jwt_decode from "jwt-decode";
 
 export default function ProtectLogin({ children, userRole }) {
   const auth = Cookies.get("mysmk_token");
-  let roles = jwt_decode(Cookies.get("mysmk_token"));
+  let roles
+ if(auth){
+   roles = jwt_decode(Cookies.get("mysmk_token"));
+ }
   return auth === undefined ? children : <Navigate to={roles?.role === "Santri" ? "/siswa/dashboard" : "/guru/dashboard"} />;
 }
 //
